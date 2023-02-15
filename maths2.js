5dea929b4dda9b9fbac948aa54b31930206d1811
yatharthw <121886758+SaxenaVaibhav80@users.noreply.github.com>
2023-02-16 00:21:43 +0530

    maths2

---
+++ maths2.js
@@ -0,0 +1,16 @@
+b=document.getElementById("dark")
+b.addEventListener("click",function()
+{
+    document.querySelector("body").classList.add("dark-back")
+    // b.style.color="white"
+    document.getElementById("light").style.color="white"
+    document.querySelector("samp").innerHTML="Light mode"
+
+})
+document.getElementById("light").addEventListener("click",function()
+{
+    document.querySelector("body").classList.remove("dark-back")
+    document.querySelector("samp").innerHTML="Dark mode"
+
+
+})
