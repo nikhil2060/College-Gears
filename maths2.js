1b5192d2093489fbddac4b87bcfbb05d55da9959
yatharthw <121886758+SaxenaVaibhav80@users.noreply.github.com>
2023-02-16 15:42:35 +0530

    change

--- maths2.js
+++ maths2.js
@@ -3,6 +3,9 @@ b.addEventListener("click",function()
 {
     document.querySelector("body").classList.add("dark-back")
     // b.style.color="white"
+    for(var i=0;i<62;i++){
+        document.getElementsByClassName("module")[i].style.boxShadow="none";
+        }
     document.getElementById("light").style.color="white"
     document.querySelector("samp").innerHTML="Light mode"
 
@@ -10,7 +13,10 @@ b.addEventListener("click",function()
 document.getElementById("light").addEventListener("click",function()
 {
     document.querySelector("body").classList.remove("dark-back")
+    for(var i=0;i<62;i++){
+        document.getElementsByClassName("module")[i].removeAttribute("style");
+        }
     document.querySelector("samp").innerHTML="Dark mode"
-
+    
 
 })
--- tutorialmath.js
+++ tutorialmath.js
@@ -3,6 +3,9 @@ b.addEventListener("click",function()
 {
     document.querySelector("body").classList.add("dark-back")
     // b.style.color="white"
+    for(var i=0;i<94;i++){
+    document.getElementsByClassName("module")[i].style.boxShadow="none";
+    }
     document.getElementById("light").style.color="white"
     document.querySelector("samp").innerHTML="Light mode"
 
@@ -10,5 +13,8 @@ b.addEventListener("click",function()
 document.getElementById("light").addEventListener("click",function()
 {
     document.querySelector("body").classList.remove("dark-back")
+    for(var i=0;i<94;i++){
+        document.getElementsByClassName("module")[i].removeAttribute("style");
+        }
     document.querySelector("samp").innerHTML="Dark mode"
 })
